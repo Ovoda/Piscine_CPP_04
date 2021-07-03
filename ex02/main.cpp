3fd8bb24bb363aacd4076b7c48b4a2af232ef6d6
Ovoda <cdetourrispro@gmail.com>
2021-07-03 07:42:20 +0200

    push vm

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -4,26 +4,31 @@
 
 int main()
 {
-
-    ISpaceMarine *bob = new TacticalMarine;
-    ISpaceMarine *jim = new AssaultTerminator;
-    ISquad *vlc = new Squad;
-    vlc->push(bob);
-    vlc->push(jim);
-    for (int i = 0; i < vlc->getCount(); ++i)
     {
-        ISpaceMarine *cur = vlc->getUnit(i);
-        cur->battleCry();
-        cur->rangedAttack();
-        cur->meleeAttack();
+        ISpaceMarine *bob = new TacticalMarine;
+        ISpaceMarine *jim = new AssaultTerminator;
+        ISquad *vlc = new Squad;
+        vlc->push(bob);
+        vlc->push(jim);
+        for (int i = 0; i < vlc->getCount(); ++i)
+        {
+            ISpaceMarine *cur = vlc->getUnit(i);
+            cur->battleCry();
+            cur->rangedAttack();
+            cur->meleeAttack();
+        }
+        delete vlc;
     }
-    delete vlc;
+    {
+        ISpaceMarine *bob = new TacticalMarine;
+        ISpaceMarine *jim = new AssaultTerminator;
 
-    Squad squad;
+        Squad squad;
 
-    squad.push(bob);
-    // squad.push(jim);
-    // for (int i = 0; i < squad.getCount(); ++i)
-    //     squad.getUnit(i)->battleCry();
+        squad.push(bob);
+        squad.push(jim);
+        // for (int i = 0; i < squad.getCount(); ++i)
+        //     squad.getUnit(i)->battleCry();
+    }
     return 0;
 }
